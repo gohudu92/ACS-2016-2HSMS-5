e50a518f65a7ee263d9846b82aa58383f6d54a4c
Seb95 <sebastien.perrier@devinci.fr>
2016-03-29 16:41:13 +0200

    Mise à jour du 29_03

---
+++ Html/script/script.js
@@ -0,0 +1,114 @@
+﻿// variables
+var page= 'aucune';
+
+// fonctions initiales
+description_site();
+
+function description_site() {
+	var flow ="Bienvenu sur CerveauTraining où vous trouverez plusieurs jeux d'entraînement cérébral à découvrir "+
+	"pour toute la famille, pour les petits comme pour les grands enfants ! Ce site vous propose des jeux pour "+
+	"tester votre mémoire et votre rapidité de réponse. Il y en a pour tout les goûts: jeu de réflexion, de rapidité, "+ 
+	"de concentration, d'analyse, de reflexe ou de logique. Jouez à des jeux qui sont simples à comprendre "+
+	"mais subtils à résoudre. Découvrez comment faire travailler votre cerveau en vous amusant. Vous "+ 
+	"pourrez jouer seul ou au contraire remporter des défis contre vos amis.";
+	document.querySelector(".description_jeu").innerHTML= flow;
+}
+
+function table_navigation() {
+
+	var a = document.createElement('li');
+	a.setAttribute('role','presentation');
+	a.setAttribute('class','off');
+	a.innerHTML = "<a href='index.html'>Accueil</a>";
+	
+	var b = document.createElement('li');
+	b.setAttribute('role','presentation');
+	b.setAttribute('class','off');
+	b.innerHTML = "<a href='connexion.html'>Se Connecter</a>";
+	
+	var c = document.createElement('li');
+	c.setAttribute('role','presentation');
+	c.setAttribute('class','off');
+	c.innerHTML = "<a href='creer_compte.html'>Créer un compte</a>";
+	
+	var d = document.createElement('li');
+	d.setAttribute('role','presentation');
+	d.setAttribute('class','off');
+	d.innerHTML = "<a href='liste_jeux.html'>Liste des Jeux</a>";
+	
+	var e = document.createElement('li');
+	e.setAttribute('role','presentation');
+	e.setAttribute('class','off');
+	e.innerHTML = "<a href='informations.html'>Informations</a>";
+	
+	if(page =='index') a.setAttribute('class','active');
+	if(page =='connexion') b.setAttribute('class','active');
+	if(page =='creer_compte') c.setAttribute('class','active');
+	if(page =='liste_jeux') d.setAttribute('class','active');
+	if(page =='informations') e.setAttribute('class','active');
+
+	document.querySelector('#table_navigation').appendChild(a);
+	document.querySelector('#table_navigation').appendChild(b);
+	document.querySelector('#table_navigation').appendChild(c);
+	document.querySelector('#table_navigation').appendChild(d);
+	document.querySelector('#table_navigation').appendChild(e);
+}
+
+// Mosaique
+var nombreDeTour = 0;
+var stockImge = "";
+var countTIM = 30;
+//var counter = setInterval(timerCount,1000);
+var counter = setInterval(prochaineEtape,5000);
+
+function prochaineEtape() {
+			
+			for(var i=0;i<6;i++)
+			{
+				var chemin = '1imgMosa'+i;
+				var image = document.getElementById(chemin);
+				var j = (i+1+nombreDeTour)%6;
+				chemin = 'images/photo'+j+'.png';
+				image.src = chemin;
+			}
+			for(var i=0;i<6;i++)
+			{
+				var chemin = '2imgMosa'+i;
+				var image = document.getElementById(chemin);
+				var j = (i+3+nombreDeTour)%6;
+				chemin = 'images/photo'+j+'.png';
+				image.src = chemin;
+			}
+			for(var i=0;i<6;i++)
+			{
+				var chemin = '3imgMosa'+i;
+				var image = document.getElementById(chemin);
+				var j = (i+4+nombreDeTour)%6;
+				chemin = 'images/photo'+j+'.png';
+				image.src = chemin;
+			}
+			nombreDeTour++
+						}
+	
+function jeSuisDessu(id) {
+		var image = document.getElementById(id);
+		if(image.src.match("dessus"))
+		{
+			image.src = stockImge;
+		}
+		else
+		{
+			stockImge = image.src;
+			image.src="images/dessus.png";
+		}
+		
+	}
+	
+function timerCount() {
+  		countTIM=countTIM-1;
+  		if (countTIM <= -1)
+  		{
+    		 clearInterval(counter);
+    	 	return;
+  		}		
+	}
---
+++ README.txt
@@ -0,0 +1,42 @@
+# ACS-2016-2HSMS-5
+
+# SP 26/03/16 : Ajout de la mosaique d'Hugo (adaptation à l'HTML, sans références aux jeux pour l'instant)
+		Ajout du script pour la génération des pages
+		Ajout des images dans le dossier "images"
+		Création de la branche 'Principale'
+
+# SP 12/03/16 : A regarder :
+		- les modifications (dans documents)
+		- le 'travail_a_faire'
+
+# SP 12/03/16 : Ajout :
+		- Dans "Documents" :
+			* Dossier "Jeux" Ajout de : 
+			'1_liste_idee_jeux'
+			   => ayant pour objectif d'y mettre une description
+			   par jeu (avec le gain de points, timer etc. en
+			   fonction de l'avancement)
+			Dossier "exemple" avec le fichier txt type
+			   => contiendra le code et le fichier texte
+			   (html,css,js pour incorporer sur le site)
+			* 'travail_a_faire'
+		- Déplacement de 'description_page_accueil' dans le dossier
+		  "Page accueil" dans "Documents"
+
+# SP 25/02/16 : Ajout :
+		- Images annexes V1
+		- Description page accueil V1
+
+# SP 22/02/16 : Point Projet 1 
+		- Gestion d'identité
+		- Choix Technologie C/S
+		- BDD Design qui est le test pendant l'élaboration : 
+			* élaboration de tests unitaires et de code 
+			  "technique" d'une part, l'élaboration de tests
+			  fonctionnels (servant à formaliser les exigences)
+			  et de "fonctionnalités" d'autre part.
+		- OUTPUT AddValue (pour le client)
+
+# SP 21/02/16 : Ajout de l'HTML + CSS (dossier de base)
+
+		
